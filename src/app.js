e4614ebb04853b45dfb3c4e7e00755701511be5d
Kelthaz <davidkelthaz@gmail.com>
2022-12-14 16:18:16 -0500

    COrs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ import {PORT} from './config.js'
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.get('/', async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM users')
   res.json(rows)
